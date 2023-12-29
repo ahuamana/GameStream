a5362a9fad27a7abfd165a51ee8dfb884c8f099a
ahuamana <73962071@continental.edu.pe>
2023-12-29 07:47:49 -0500

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Antony Huaman Alikhan on 29/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
