e2ba2d45ff826aaa6cf219b38e5a223ea19017bd
ahuamana <73962071@continental.edu.pe>
2024-01-04 00:42:59 -0500

    FEATURE - HOME MOST VIDEOS LIKELY

--- GameStream/Home.swift
+++ GameStream/Home.swift
@@ -12,12 +12,19 @@ struct Home: View {
     @State var tabSeleccionado: Int = 2
     
     init() {
-        UITabBar.appearance().backgroundColor = UIColor(Color("TabBar-Color"))
+        let appearance = UITabBarAppearance()
+        appearance.backgroundColor = UIColor(Color("TabBar-Color"))
         
+        
+        
+        UITabBar.appearance().backgroundColor = UIColor(Color("TabBar-Color"))
         UITabBar.appearance().unselectedItemTintColor = UIColor.gray
+        UITabBar.appearance().standardAppearance = appearance
+        //UITabBar.appearance().scrollEdgeAppearance = appearance
     }
     
     var body: some View {
+        
         TabView(selection: $tabSeleccionado) {
             
             Text("Perfil")
@@ -54,49 +61,51 @@ struct HomeChild : View {
     
     var body : some View {
         ZStack {
-            
+        
             Color("Marine").ignoresSafeArea()
             
-            VStack() {
-               
-                //Header
-                Image("appLogo")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 250)
-                    .padding(.horizontal, 11)
-                
-                Spacer().frame(height: 28)
-                
-                // Componente buscar
-                HStack {
-                    Button(action: busqueda, label: {
-                        Image(systemName: "magnifyingglass")
+                ScrollView {
+                    VStack() {
+                       
+                        //Header
+                        Image("appLogo")
                             .resizable()
-                            .foregroundColor(textoBusqueda.isEmpty ? .yellow : Color("Dark-Cian"))
-                            .frame(width: 25, height: 25)
                             .aspectRatio(contentMode: .fit)
-                    })
-                    
-                    ZStack(alignment: .leading) {
+                            .frame(width: 250)
+                            .padding(.horizontal, 11)
                         
-                        if textoBusqueda.isEmpty {
-                            Text ("Buscar un video").foregroundColor(Color(red: 174/255, green: 177/255, blue: 185/225, opacity:1))
-                                .font(.system(size: 14))
-                        }
+                        Spacer().frame(height: 28)
                         
-                        TextField("", text: $textoBusqueda).foregroundColor(.white)
-                            .font(.system(size: 14))
+                        // Componente buscar
+                        HStack {
+                            Button(action: busqueda, label: {
+                                Image(systemName: "magnifyingglass")
+                                    .resizable()
+                                    .foregroundColor(textoBusqueda.isEmpty ? .yellow : Color("Dark-Cian"))
+                                    .frame(width: 25, height: 25)
+                                    .aspectRatio(contentMode: .fit)
+                            })
+                            
+                            ZStack(alignment: .leading) {
+                                
+                                if textoBusqueda.isEmpty {
+                                    Text ("Buscar un video").foregroundColor(Color(red: 174/255, green: 177/255, blue: 185/225, opacity:1))
+                                        .font(.system(size: 14))
+                                }
+                                
+                                TextField("", text: $textoBusqueda).foregroundColor(.white)
+                                    .font(.system(size: 14))
+                                
+                            }
+                        }.padding([.bottom, .top, .leading], 11)
+                            .background(Color("Blue-Gray"))
+                            .clipShape(Capsule())
                         
-                    }
-                }.padding([.bottom, .top, .leading], 11)
-                    .background(Color("Blue-Gray"))
-                    .clipShape(Capsule())
-                
-                //Body
-                SubModuleHome()
-                
-            }.padding(.horizontal, 18).frame(maxHeight: .infinity, alignment: .top)
+                        SubModuleHome()
+                        
+                        
+                    }.padding(.horizontal, 18).frame(maxHeight: .infinity, alignment: .top)
+                }
             
         }.navigationBarHidden(true)
         .navigationBarBackButtonHidden(true)
@@ -134,11 +143,15 @@ struct SubModuleHome : View {
                     VStack(spacing: 0) {
                         Image("image 20")
                             .resizable()
-                            .scaledToFit()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(maxWidth: .infinity)
+                        
                         Text("Uncharted 4")
                             .frame(maxWidth: .infinity, alignment: .leading)
-                            .padding([.top, .bottom], 6)
+                            .padding([.horizontal])
+                            .padding([.top, .bottom], 13)
                             .background(Color("Blue-Gray"))
+                            .cornerRadius(6, corners:[.bottomLeft, .bottomRight])
                     }
                 })
                 
@@ -151,16 +164,168 @@ struct SubModuleHome : View {
             }.frame(maxWidth: .infinity, alignment: .center)
                 .padding(.vertical)
             
+            //Categorias sugeridas para ti
             
-                .navigationDestination(isPresented: $isPlayerActive) {
-                    VideoPlayer(player: AVPlayer(url: URL(string: url)!))
+            Text("CATEGORÍAS SUGERIDAS PARA TI")
+                .font(.title3)
+                .foregroundColor(.white)
+                .bold()
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    
+                    Button(action: goToFPS, label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color("Blue-Gray"))
+                                .frame(width: 160, height: 90)
+                            VStack {
+                                Image("ic_fps")
+                                Text("FPS")
+                                    .font(.body)
+                                    .bold()
+                                    .foregroundColor(Color("Dark-Cian"))
+                            }
+                        }
+                    }).padding(.trailing)
+                    
+                    Button(action: goToFPS, label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color("Blue-Gray"))
+                                .frame(width: 160, height: 90)
+                            VStack {
+                                Image("ic_rpg")
+                                Text("RPG")
+                                    .font(.body)
+                                    .bold()
+                                    .foregroundColor(Color("Dark-Cian"))
+                            }
+                        }
+                    }).padding(.trailing)
+                    
+                    Button(action: goToFPS, label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color("Blue-Gray"))
+                                .frame(width: 160, height: 90)
+                            VStack {
+                                Image("ic_open_world")
+                                Text("OPEN WORLD")
+                                    .font(.body)
+                                    .bold()
+                                    .foregroundColor(Color("Dark-Cian"))
+                            }
+                        }
+                    }).padding(.trailing)
                 }
+            }
+            
+            //Categorias recomendados
+            Text("RECOMENDADOS PARA TI")
+                .font(.title3)
+                .foregroundColor(.white)
+                .bold()
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.top)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    
+                    Button(action: {
+                        url = urlVideos[1]
+                        print("url: \(url)")
+                    }, label: {
+                        Image("image 22")
+                            .resizable()
+                            .frame(width: 227, height: 128)
+                        .cornerRadius(8)})
+                    
+                    Button(action: {
+                        url = urlVideos[2]
+                        print("url: \(url)")
+                    }, label: {
+                        Image("image 21")
+                            .resizable()
+                            .frame(width: 227, height: 128)
+                        .cornerRadius(8)})
+                    
+                    Button(action: {
+                        url = urlVideos[3]
+                        print("url: \(url)")
+                    }, label: {
+                        Image("image 23")
+                            .resizable()
+                            .frame(width: 227, height: 128)
+                        .cornerRadius(8)})
+                    
+                    
+                }
+            }
+            
+            //Categorias recomendados
+            Text("VIDEOS QUE PODRIAN GUSTARTE")
+                .font(.title3)
+                .foregroundColor(.white)
+                .bold()
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.top)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    
+                    Button(action: {
+                        url = urlVideos[1]
+                        print("url: \(url)")
+                    }, label: {
+                        Image("image 25")
+                            .resizable()
+                            .frame(width: 227, height: 128)
+                        .cornerRadius(8)})
+                    
+                    Button(action: {
+                        url = urlVideos[2]
+                        print("url: \(url)")
+                    }, label: {
+                        Image("image 26")
+                            .resizable()
+                            .frame(width: 227, height: 128)
+                        .cornerRadius(8)})
+                    
+                    Button(action: {
+                        url = urlVideos[3]
+                        print("url: \(url)")
+                    }, label: {
+                        Image("image 27")
+                            .resizable()
+                            .frame(width: 227, height: 128)
+                        .cornerRadius(8)})
+                    
+                    
+                }
+            }
+            
+            
+            //Main Stack
+            .navigationDestination(isPresented: $isPlayerActive) {
+                VideoPlayer(player: AVPlayer(url: URL(string: url)!))
+            }.padding(.bottom)
+            
+            
+            
+            
             
         }
         
         
         
     }
+}
+
+func goToFPS () {
+    
 }
 
 
